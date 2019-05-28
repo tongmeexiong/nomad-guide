f7aa127f0214131b4035e55026ea0ed73004568b
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-05-28 13:26:15 -0500

    added reducer and saga for explore page

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const Explore = () => (
-  <div>
-    <p>
-      Explore Page
-    </p>
-  </div>
-);
+class Explore extends React.Component{
 
-export default Explore;
+  componentDidMount(){
+    this.getExploreList()
+  }
+
+  getExploreList =()=>{
+    this.props.dispatch({type: 'FETCH_EXPLORE'})
+  }
+  render(){
+    return(
+      <p> Explore</p>
+    )
+  }
+}
+
+export default connect()(Explore);
